870c14d6532d8562db7b65e646e7645d41bfec13
kazuhe <omc.ohara@gmail.com>
2020-08-28 10:52:09 +0900

    mod : Change function to class

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const logoAttrs = {
+      alt: "logo",
+      className: "App-logo",
+      src: logo
+    };
+    const title = "Hello React!";
+    const targets = ["Nyan!", "Wan!"];
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img {...logoAttrs} />
+          {title && <p>{title}</p>}
+          {targets.map(target => (
+            <p>Hello {target}!</p>
+          ))}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
