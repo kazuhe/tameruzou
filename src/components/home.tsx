e48f40b3003c44720c9a905c2993abd63d219324
kazuhe <omc.ohara@gmail.com>
2020-09-18 02:49:39 +0900

    mod : Design adjustment

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -14,30 +14,31 @@ const Home: FC = () => {
 
   return (
     <div className="home">
-      <img className="home_image" src={tameruzou} alt="" />
-      <p className="home_subTitle">貯金シミュレーター</p>
-      <h1>ためるゾウ</h1>
-      <p className="home_text">
-        毎月いくら貯金すれば目標に到達するのか？
-        <br />
-        期間を決めてコツコツ貯めるといくら貯まるのか？
-        <br />
-        下のボタンから選択して簡単シミュレート！
-      </p>
-      <IconButton
-        icon={goal}
-        color="deep"
-        title="目標貯金額からシミュレート"
-        text="Simulate from target amount"
-        onClick={() => history.push('/simulate')}
-      />
-      <IconButton
-        icon={graph}
-        color="high"
-        title="毎月の金額からシミュレート"
-        text="Simulate from monthly amount"
-        onClick={() => history.push('/simulate')}
-      />
+      <div className="home_inner">
+        <div className="home_icon">
+          <img src={tameruzou} alt="" />
+        </div>
+        <h1 className="home_title">貯金シミュレーター</h1>
+        <p className="home_text">
+          Let&apos;s simulate
+          <br />
+          the future
+        </p>
+        <IconButton
+          icon={goal}
+          color="deep"
+          title="目標貯金額からシミュレート"
+          text="Simulate from target amount"
+          onClick={() => history.push('/simulate')}
+        />
+        <IconButton
+          icon={graph}
+          color="high"
+          title="毎月の金額からシミュレート"
+          text="Simulate from monthly amount"
+          onClick={() => history.push('/simulate')}
+        />
+      </div>
     </div>
   )
 }
